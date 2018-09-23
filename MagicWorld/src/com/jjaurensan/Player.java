1f0cb658e7caf10590bc96bbace892ffe982147d
jjaurensan <jj.aurensan@gmail.com>
2018-09-23 18:55:16 +0200

    Redactions et MaJ des JAVADOC ainsi que des commentaires

--- MagicWorld/src/com/jjaurensan/Player.java
+++ MagicWorld/src/com/jjaurensan/Player.java
@@ -57,31 +57,43 @@ public class Player {
 	
 	/**
 	 * Display and Ask the level for futur character
+	 * Affiche et demande le niveau du personnage
+	 * Niveau : choisi par le joueur (min 1, max 100)
 	 */	
-	public void askLevel() {	//Niveau : choisi par le joueur (min 1, max 100)
+	public void askLevel() {
 		System.out.println("Niveau du personnage ?");
 		do {
 			this.playerLevel = sc.nextInt();
 		} while (this.playerLevel < 1 || this.playerLevel > 100 );	
 	}
 	/**
-	 * Display and Ask the force for the futur Character
-	 */
-	public void askForce() {	//Force : choisie par le joueur (min 0, max 100)
+	 * Display and Ask the force for futur character
+	 * Affiche et demande la force du personnage
+	 * Force : choisie par le joueur (min 0, max 100)
+	 */	
+	public void askForce() {
 		System.out.println("Force du personnage ? (Reste "+this.playerLevel+" points à placer)");
 		do {
 			this.playerForce = sc.nextInt();
 		} while (this.playerForce < 0 || this.playerForce > this.playerLevel );	
 	}
-	
-	public void askAgility() {	//Agilité : choisie par le joueur (min 0, max 100)
+	/**
+	 * Display and Ask the agility for futur character
+	 * Affiche et demande la agilité du personnage
+	 * Agilité : choisie par le joueur (min 0, max 100)
+	 */	
+	public void askAgility() {
 		System.out.println("Agilité du personnage ? (Reste "+(this.playerLevel-this.playerForce)+" points à placer)");
 		do {
 			this.playerAgility = sc.nextInt();
 		} while (this.playerAgility < 0 || this.playerAgility > (this.playerLevel-this.playerForce) );
 	}
-	
-	public void askIntelligence() {	//Intelligence : choisie par le joueur (min 0, max 100)
+	/**
+	 * Display and Ask the intelligence for futur character
+	 * Affiche et demande l'intelligence du personnage
+	 * Intelligence : choisie par le joueur (min 0, max 100)
+	 */	
+	public void askIntelligence() {
 		System.out.println("Intelligence du personnage ? (Reste "+(this.playerLevel-this.playerForce - this.playerAgility)+" points à placer)");
 		do {
 			this.playerIntelligence = sc.nextInt();
@@ -91,9 +103,14 @@ public class Player {
 	
 	
 	
-	
+	/**
+	 * display Character feature
+	 * Affiche une phrase recapitulative des caracteristiques du personnage et sert aussi de confirmation de creation
+	 * 
+	 * Template to use /Modele à utiliser : "CRI DE GUERRE" je suis le "TYPE PERSO" "JOUEUR" niveau XX je possede XX de vitalité, XX de force, XX d'agilité et XX d'intelligence !
+	 * 
+	 */
 	public void displayCharacterCreationConfirmation() {
-		//"CRI DE GUERRE" je suis le "TYPE PERSO" "JOUEUR" niveau XX je possede XX de vitalité, XX de force, XX d'agilité et XX d'intelligence !
 		String creationConfirmation = this.playerCharacter.getWarCry()
 				+" je suis le "+this.playerCharacter.getTypeCharacter()+" "+this.playerName
 				+" niveau "+this.playerCharacter.getLevel()
@@ -108,12 +125,18 @@ public class Player {
 		this.enemy = enemy;
 		//"JOUEUR" ("LIFE") veuillez choisir votre action (1 : Attaque Basique, 2 : Attaque Spéciale)
 	}
-	
+	/**
+	 * Name of the player / Nom du joueur
+	 * @return the player name
+	 */
 	public String getPlayerName() {
 		return playerName;
 	}
 
-
+	/**
+	 * Name of the player / Nom du joueur
+	 * @param playerName the player name
+	 */
 	public void setPlayerName(String playerName) {
 		this.playerName = playerName;
 	};
