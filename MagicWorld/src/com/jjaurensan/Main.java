9e2207a05458981af8d605098f09a50b711920d9
jjaurensan <jj.aurensan@gmail.com>
2018-09-23 18:38:40 +0200

    un peu de code pour tester la creation du personnage

--- MagicWorld/src/com/jjaurensan/Main.java
+++ MagicWorld/src/com/jjaurensan/Main.java
@@ -2,5 +2,9 @@ package com.jjaurensan;
 
 public class Main {
 	
-
+	 public static void main(String[] args) {
+	Player playerOne = new Player();
+	playerOne.setPlayerName("Joueur 1");
+	playerOne.createNewCharacter();
 	}
+}
